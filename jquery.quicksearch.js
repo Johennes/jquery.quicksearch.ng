1dbbab7464872e532535a1898e980cbf542d8a65
Johannes Marbach <JohannesMarbach@gmail.com>
2015-03-08 11:12:48 +0100

    Small refactorings and comments

--- jquery.quicksearch.js
+++ jquery.quicksearch.js
@@ -42,8 +42,6 @@
 		var addToRowSpan = function($node, amount) {
 			$node.attr("rowspan", parseInt($node.attr("rowspan")) + amount);
 		};
-			
-			
 		
 		this.go = function () {
 			var i = 0,
@@ -64,105 +62,110 @@
 					options.show.apply(rowcache[i]);
 					
 					e.doIfString(options.rowspanselector, function() {
-						// Check if this element has the rowspan
-						var rs = $(rowcache[i]).find(options.rowspanselector);
-						if (rs.length !== 0) {
-							addToRowSpan(rs, 1);
+						// Check if this row has the rowspan. If so, we can just increment it and are done.
+						var $rs = $(rowcache[i]).find(options.rowspanselector);
+						if ($rs.length !== 0) {
+							addToRowSpan($rs, 1);
 							return;
 						}
 						
-						// Check for rowspan on any of the following rows
+						var group = $(rowcache[i]).attr(options.rowspangroupattribute);
+						
+						// Check if any of the following rows has the rowspan. Note that the rowspan might be
+						// on a hidden row if all rows of this group were hidden.
 						for (var j = i + 1; j < len; ++j) {
 							$node = $(rowcache[j]);
 							
-							if ($node.length === 0
-								|| $node.attr(options.rowspangroupattribute) !== $(rowcache[i]).attr(options.rowspangroupattribute)) {
-								break;
+							if ($node.attr(options.rowspangroupattribute) !== group) {
+								break; // We've reached the end of this group
 							}
 							
-							// The row span might be on a hidden row if all rows for this group were hidden
-							/*if (isHidden($node)) {
-								continue;
-							}*/
-							
-							rs = $node.find(options.rowspanselector);
-							if (rs.length !== 0) { // Move and increment rowspan
-								rs.detach().prependTo($(rowcache[i]));
-								addToRowSpan(rs, 1);
+							$rs = $node.find(options.rowspanselector);
+							if ($rs.length !== 0) {
+								// We've found the rowspan. Increment it and move it to the shown row.
+								addToRowSpan($rs, 1);
+								$rs.detach().prependTo($(rowcache[i]));
+								// TODO: This will only work if the rowspan element is the first
+								// child inside the row.
 								return;
 							}
 						}
 						
-						// Find rowspan on preceding row
+						// Check if any of the preceding rows has the rowspan. Note that the rowspan might be
+						// on a hidden row if all rows of this group were hidden.
 						for (var j = i - 1; j >= 0; --j) {
 							$node = $(rowcache[j]);
 							
-							if ($node.length === 0
-								|| $node.attr(options.rowspangroupattribute) !== $(rowcache[i]).attr(options.rowspangroupattribute)) {
-								break;
+							if ($node.attr(options.rowspangroupattribute) !== group) {
+								break; // We've reached the end of this group
 							}
 							
-							// The row span might be on a hidden row if all rows for this group were hidden
-							/*if (isHidden($node)) {
-								continue;
-							}*/
-							
-							rs = $node.find(options.rowspanselector);
-							if (rs.length !== 0) { // Move and increment rowspan
-								addToRowSpan(rs, 1);
+							$rs = $node.find(options.rowspanselector);
+							if ($rs.length !== 0) {
+								// We've found the rowspan. Increment it. No need to move it since it already
+								// is on a preceding row.
+								addToRowSpan($rs, 1);
 								return;
 							}
 						}
 					});
 				} else {
-					if (options.isHidden(rowcache[i])) {
+					if (options.isHidden(rowcache[i])) { // Only hide rows that are visible
 						continue;
 					}
 						
 					options.hide.apply(rowcache[i]);
 					
 					e.doIfString(options.rowspanselector, function() {
-						var rs = $(rowcache[i]).find(options.rowspanselector);
-						if (rs.length !== 0) {
-							addToRowSpan(rs, -1);
+						var group = $(rowcache[i]).attr(options.rowspangroupattribute);
+						
+						// Check if this row has the rowspan. If so, we need to move it to the next visible row.
+						var $rs = $(rowcache[i]).find(options.rowspanselector);
+						if ($rs.length !== 0) {
+							addToRowSpan($rs, -1);
 							
-							// Find next visible element
+							// Find next visible row
 							for (var j = i + 1; j < len; ++j) {
 								$node = $(rowcache[j]);
 								
-								if ($node.length === 0
-									|| $node.attr(options.rowspangroupattribute) !== $(rowcache[i]).attr(options.rowspangroupattribute)) {
-									break;
+								if ($node.attr(options.rowspangroupattribute) !== group) {
+									break; // We've reached the end of this group
 								}
 								
 								if (options.isHidden($node[0])) {
-									continue;
+									continue; // Ignore hidden nodes
 								}
 								
-								// Move element to node and decrement rowspan
-								rs.detach().prependTo($node);
+								// We've found a visible row. Move the rowspan inside it.
+								$rs.detach().prependTo($node);
+								// TODO: This will only work if the rowspan element is the first
+								// child inside the row.
 								break;
 							}
 						} else {
-							// Find rowspan on previous row
+							// Check if any of the preceding rows has the rowspan
 							for (var j = i - 1; j > 0; --j) {
 								$node = $(rowcache[j]);
 								
-								if ($node.length === 0
-									|| $node.attr(options.rowspangroupattribute) !== $(rowcache[i]).attr(options.rowspangroupattribute)) {
-									break;
+								if ($node.attr(options.rowspangroupattribute) !== group) {
+									break; // We've reached the end of this group
 								}
 								
 								if (options.isHidden($node[0])) {
-									continue;
+									continue; // Ignore hidden nodes
 								}
 								
-								rs = $node.find(options.rowspanselector);
-								if (rs.length !== 0) { // Decrement rowspan								
-									addToRowSpan(rs, -1);
+								$rs = $node.find(options.rowspanselector);
+								if ($rs.length !== 0) {
+									// We've found the rowspan. Decrement it.
+									addToRowSpan($rs, -1);
 									break;
 								}
 							}
+							
+							// We don't need to search for the rowspan on any of the
+							// following rows since it will always be on the first
+							// visible row inside a group and this row was visible.
 						}
 					});
 				}
